16156e2b56517644d77f1c14289176080698526c
giovanirogerio02 <Geovannirogeriogr@gmail.com>
2024-08-23 17:18:41 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,118 @@
+
+<!DOCTYPE html>
+  <html lang="pt-BR">
+
+  <head>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">    
+      <title>As notícias</title>
+      <link rel="stylesheet" href="style.css">
+  </head>
+
+  <body>
+
+    <a href="https://grjornalismo.blogspot.com/">Confira no Blog</a> 
+
+    Exemplo: 
+    <a href="https://grjornalismo.blogspot.com/">A notícia</a>
+
+
+    <button>As notícias</button>
+
+    <a href="https://www.threads.net/@geovanne_fotos?hl=it">Volte para a página inicial</a>
+
+    <p style=" color: rgb(4, 67, 126); ">
+      As notícias são como são e acontecem 24 horas.         
+   </p>
+
+   <style type="text/css">
+            
+    p {
+        color: rgb(39, 92, 160);
+    }
+
+    h1 {
+        color: blue;
+    }
+</style>
+
+<style type="text/css">
+            
+  .azul {
+      color: blue;
+  }
+
+  #cabecalho {
+      background: green;
+  }
+body {
+      background-color: rgb(206, 153, 54);
+  }  
+
+
+</style>
+
+
+    <div class="container">
+      <div class="row">
+
+        <div class="col-sm">
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh0j8g5P2ezVNJREazsbVUkWDRTfWp088viREE9mhNyTglUObi3M27_45Qok1OvsXE4X_lZ2fYvpw_z0DKpOHUbGVuUwWOq91FfiGw4ASf7Q_BNmZFKFZF6Vwg_yoJXNnD80GhFK9JU-zed/s2048/IMG-20210905-WA0094.jpeg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Os incendios</h5>
+              <p class="card-text">Os incendios acontecem a todo tempo.</p>
+              <a href="https://www.youtube.com/watch?v=Fg0WRhylyjE" class="btn btn-primary">O incendio</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm">
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgf68PiyvUN9VeBqlM9odekYA26CyyIs372DgRji4x1GIS3mE0s3W89molg9n6-Bbq2h48n0RupY4fCWQvoPxFIrMJlaSzBbBgeUm__tVRi0PxGQfZfCC5DjTq4yOwQteN8Y0aVY8yy3QzpQvbZQJVI3F1Horde-f4eB-pCz1ABt1RIwUKQc734vxP1LJl7/s303/download.jpeg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">A seca</h5>
+              <p class="card-text">Um dos grandes problemas da humanidade.</p>
+              <a href="https://www.youtube.com/watch?v=ZLmT5_EIOSE&t=1s" class="btn btn-primary">A seca</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm">
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgOyRDYKBNaLqC0-6YhoFl2VcpRwVeqqOCqVYJhwKss6JvfLV3-N58w7uS_Cxrl3B4_ctvNK1AA03HFPCPVQ0uNDV8ro6z_a1FSbMBfTmZ_lqtbGpS40NBMFKdzEef07rEY1hbn4b1tmGVPg5QDXfq1kLiNkYJpN7mpccOInI4_8M33IjJRHs-1a7HIDDZZ/s276/download%20(1).jpeg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">O lixo</h5>
+              <p class="card-text">Um dos problemas que ninguém resolve.</p>
+              <a href="https://www.youtube.com/watch?v=t2AEK7UN0a8" class="btn btn-primary">O lixo</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm">
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhuG6a78rqXKFrn03sPvPAXEGHbtsV0lB187-NJ5MFYJAHZl3uNx8K_Nn2sxI2nDZ-wJ3TWAkYalk2I4Jy8eOZtMv0um0nl7neR3ogEKbeOuePZFIs8wy6_pkb13L1KiGaydOsrQuNTx_d0KEIYYcVHiXVVhQCxl1-aU0fFaI594N8z-_bmbkqvQlA8VyGo/s277/download%20(1).jpeg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">A vacina</h5>
+              <p class="card-text">A vacina, salva vidas.</p>
+              <a href="https://www.youtube.com/watch?v=O4De52sS9Ko" class="btn btn-primary">A vacina</a>
+            </div>
+          </div>
+        </div>
+
+        
+
+      </div>
+    </div>
+
+ 
+    <a href="https://www.youtube.com/@JornalismoEsportivo" target="_blank">Visite este site</a>
+
+    
+    
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    </body>
+  </html>
